29e8078d3240e017df4e9d374d65addfaf42c81e
Rachit Garg <rachitgarg900@gmail.com>
2022-04-24 19:59:48 +0200

    Added methods and associated functions to rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,20 +4,52 @@
 struct Rectangle {
     width: u32,
     height: u32    
+}
+
+// All the associated functions (methods and non methods) are defined under impl blocks
+// multiple impl blocks allowed
+// self parameter is compulsory for methods
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    // methods can have same name as fields
+    fn width(&self) -> bool {
+        self.width > 0
+   }
 } 
 
+impl Rectangle {
+    // method with additional parameters 
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+
+    // A non method associated function
+    // self is not required in this case as it is like a static function in Java or C++
+    fn square(width: u32) -> Rectangle {
+        Rectangle {
+            width,
+            height: width
+        }
+    }
+}
+
 fn main() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50
     };        
 
-    println!("\nThe area of rectangle is: {} square units", area(&rect));
+    // Calling a method of Rectangle instance
+    println!("\nThe area of rectangle is: {} square units", rect1.area());
+    println!("This rectangle has a non-zero width: {}", rect1.width());
 
-    println!("\nrect is : {:#?}", rect);
+    println!("\nrect1 is : {:#?}", rect1);
 
     // Another alternative is dbg! macro 
-    dbg!(&rect);
+    dbg!(&rect1);
 
     println!("");
 
@@ -30,10 +62,22 @@ fn main() {
         height: 20
     };
 
-    dbg!(&rect2);
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect3 = Rectangle {
+        width: 10,
+        height: 30
+    };
+
+    println!("\nrect1 can hold rect2 : {}", rect1.can_hold(&rect2));
+    println!("rect1 can hold rect3 : {}", rect1.can_hold(&rect3));
+    println!("");
+    dbg!(&rect2);
+
+
+    // Calling a non method associated function
+    let square = Rectangle::square(50);
+
+    println!("");
+    dbg!(&square);
 }
 
